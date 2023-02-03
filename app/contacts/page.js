5349f9029c1df804acde995425b395698b8a7433
AirP0WeR <mgyp@mail.ru>
2023-02-03 18:17:11 +0300

    add recapcha

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,43 +1,74 @@
-'use client'
-import { useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+"use client";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import contacts from "/public/images/contacts/contacts.jpg";
-
+import ReCAPTCHA from "react-google-recaptcha";
+import React from "react";
 
 export default function Page() {
-
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [message, setMessage] = useState('');
-const toSend = {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const toSend = {
     name: name,
     email: email,
-    message: message
-}
+    message: message,
+  };
+  const recaptchaRef = React.createRef();
 
-const notifySuccess = () => toast.success('Сообщение успешно отправлено!');
-const notifyFail = () => toast.error('Что-то пошл не так!');
 
-async function sendTelegrammMessage() {
-    const TELEGRAM_TOKEN  = process.env.NEXT_PUBLIC_TELEGRAM_TOKEN;
+  const notifySuccess = () => toast.success("Сообщение успешно отправлено!");
+  const notifyFail = () => toast.error("Что-то пошл не так!");
 
-         await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=175042839&text=${JSON.stringify(toSend)}`)
-         .then(response => response.json())
-         .then(result => {
-            if (result.ok === true) {
-                notifySuccess();
-            } else {
-                notifyFail();
-            }
-         })
- }
+  async function sendTelegrammMessage() {
+    const TELEGRAM_TOKEN = process.env.NEXT_PUBLIC_TELEGRAM_TOKEN;
 
+    await fetch(
+      `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage?chat_id=175042839&text=${JSON.stringify(
+        toSend
+      )}`
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.ok === true) {
+          notifySuccess();
+        } else {
+          notifyFail();
+        }
+      });
+  }
+
+  const handleSubmit = () => {
+
+    console.log(recaptchaRef.current);
+
+  
+  };
+
+  const onReCAPTCHAChange = (captchaCode) => {
+    // If the reCAPTCHA code is null or undefined indicating that
+    // the reCAPTCHA was expired then return early
+    if (!captchaCode) {
+
+
+      return;
+    }
+    // Else reCAPTCHA was executed successfully so proceed with the
+    // alert
+    alert(`Hey`);
+    sendTelegrammMessage();
+    // Reset the reCAPTCHA so that it can be executed again if user
+    // submits another email.
+    recaptchaRef.current.reset();
+  };
 
   return (
     <div className="container">
-    <Toaster  containerStyle={{
-    top: 400
-  }}/>
+      <Toaster
+        containerStyle={{
+          top: 400,
+        }}
+      />
 
       <div
         className="row justify-content-center contactsContainer"
@@ -52,58 +83,62 @@ async function sendTelegrammMessage() {
         >
           <div>
             <div className="mb-3">
-              <label className="form-label">
-                Имя
-              </label>
+              <label className="form-label">Имя</label>
               <input
                 name="name"
                 type="text"
                 className="form-control"
                 placeholder="Ваше имя"
                 value={name}
-                onChange={e => setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
               />
             </div>
 
             <div className="mb-3">
-              <label className="form-label">
-                Email
-              </label>
+              <label className="form-label">Email</label>
               <input
                 name="email"
                 type="text"
                 className="form-control"
                 placeholder="name@example.com"
                 value={email}
-                onChange={e => setEmail(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
 
             <div className="mb-3">
-              <label className="form-label">
-                Сообщение
-              </label>
+              <label className="form-label">Сообщение</label>
               <textarea
                 name="message"
                 className="form-control"
                 rows="3"
                 value={message}
-                onChange={e => setMessage(e.target.value)}
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
             </div>
-            <button
-              className="btn btn-secondary"
-              style={{ backgroundColor: "#929497" }}
-              onClick={() => {
-                setName('Ваше имя');
-                setEmail('name@example.com');
-                setMessage('');
-                sendTelegrammMessage();
-            }}
-            >
-              Отправить
-            </button>
-
+            <div className="row">
+              <div className="col">
+                <button
+                  className="btn btn-secondary mt-4"
+                  style={{ backgroundColor: "#929497" }}
+                  onClick={() => {
+                    setName("Ваше имя");
+                    setEmail("name@example.com");
+                    setMessage("");
+                    handleSubmit();
+                  }}
+                >
+                  Отправить
+                </button>
+              </div>
+              <div className="col mt-4">
+              <ReCAPTCHA
+                  ref={recaptchaRef}
+                  sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY_LOCAL}
+                  onChange={onReCAPTCHAChange}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
