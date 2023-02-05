ea3128363e0ba961072b18f3be4df521d5470ceb
AirP0WeR <mgyp@mail.ru>
2023-02-05 21:02:04 +0300

    bug fix

--- app/page.js
+++ app/page.js
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
-import Head from "next/head";
 
 
 export default function Home() {
   return (
     <>
-    <Head />
-
-
     <div className="album py-2">
       <div className="container">
         <div className="row row-cols-1 row-cols-sm-2 g-4">
