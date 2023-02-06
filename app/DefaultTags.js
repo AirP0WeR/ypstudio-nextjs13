99eb64045a793f23dd3264badb3a864754e2f1f0
AirP0WeR <mgyp@mail.ru>
2023-02-06 12:18:48 +0300

    add g-analitycs

--- app/DefaultTags.js
+++ app/DefaultTags.js
@@ -1,6 +1,8 @@
+import Script from 'next/script'
 
 
 export default function DefaultTags() {
+  const GA_MEASUREMENT_ID =  process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   return (
     <>
         <meta charSet="utf-8"/>
@@ -8,6 +10,22 @@ export default function DefaultTags() {
         <meta name="description" content="Дизайн студия Юлии Пережогиной. Логотипы, Бренд Бук, Графика, Картины."/>
         <meta name="author" content="Yulia Perezhogina"/>
         <link rel="shortcut icon" href='/images/favicon.ico' type="ico"/>
+
+
+                {/* <!-- Google tag (gtag.js) --> */}
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive"></Script>
+
+
+        <Script id="google-analytics-script" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+
 
     </>
   )
