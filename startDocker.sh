35d0b7b2579736cb987f20f10e15517f550b619a
AirP0WeR <mgyp@mail.ru>
2023-03-18 15:18:00 +0300

    remove sh file

--- startDocker.sh
+++
@@ -1,4 +0,0 @@
-docker stop ypstudio
-docker container rm ypstudio
-docker build -t ypstudio-nextjs .
-docker run -p 3000:3000 --name ypstudio -d --network mongodb-init-docker_db --restart always ypstudio-nextjs
